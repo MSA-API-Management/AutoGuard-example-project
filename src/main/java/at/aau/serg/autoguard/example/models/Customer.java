46f1b5fac33f439431b2d2621a7979a3bdaecb6a
David Laubreiter <david.laubreiter@outlook.com>
2025-01-23 10:21:32 +0100

    feat: name must not be null

--- src/main/java/at/aau/serg/autoguard/example/models/Customer.java
+++ src/main/java/at/aau/serg/autoguard/example/models/Customer.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,6 +13,7 @@ public class Customer {
 
     private Long id;
 
+    @NotNull
     private String name;
 
     private String email;
